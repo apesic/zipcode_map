63f7f8a0f9226c603f4f8aa6447c18b25dade936
Alexei Pesic <aly.pesic@gmail.com>
2014-11-06 14:54:46 -0800

    Setup DB and routes

--- config/application.rb
+++ config/application.rb
@@ -8,6 +8,7 @@ Bundler.require(*Rails.groups)
 
 module Zipmap
   class Application < Rails::Application
+    config.cache_store = :memory_store, { size: 256.megabytes }
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
