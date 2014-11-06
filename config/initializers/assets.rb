d59903865e6fd2e57511fd4d6356835bd4b5b593
Alexei Pesic <aly.pesic@gmail.com>
2014-11-06 14:55:20 -0800

    Load zip shapefile into cache on initialization

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -6,3 +6,16 @@ Rails.application.config.assets.version = '1.0'
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
 # Rails.application.config.assets.precompile += %w( search.js )
+
+ZIPCACHE = ActiveSupport::Cache::MemoryStore.new
+RGEO_FACTORY = RGeo::Geographic.simple_mercator_factory
+SHAPEFILE = RGeo::Shapefile::Reader.open('zips/cb_2013_us_zcta510_500k.shp', :factory => RGEO_FACTORY)
+zips = []
+i = 0
+SHAPEFILE.each do |zip|
+  p i
+  zips << zip
+  i += 1
+  # break if i > 1000
+end
+ZIPCACHE.write('zips', zips)
